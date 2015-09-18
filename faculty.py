4959709d2c1f78b6eb6390eafe2cf683e338b8c1
Fabian Enos <fabian.enos@gmail.com>
2015-09-18 08:38:22 +0530

    Extract the faculty profile url from the department page.

--- faculty.py
+++ faculty.py
@@ -7,6 +7,41 @@ import operator
 
 
 class Miner:
+	def facultyMiner(self, url):
+		print url
+
+	'''
+		This takes the url of a department and extracts the name of the department and passes the url of the faculty to the faculty miner
+	'''
+	def departementMiner(self, url):
+		# load the html document
+		d = pq(url=url)
+
+		# get the department name
+		name = d('.civilmtext').eq(0).text()
+
+		# get the links so that we can find the link to the faculty profile
+		target = None
+		links = d('.civilmenu')('a')
+
+		# loop through the links until we find the faculty profile
+		for i in range(0, links.length):
+			text = links.eq(i).text().strip()
+
+			if text.lower() == 'faculty profile':
+				target = i
+				break
+
+		# check if we found the target url
+		if target is None:
+			print "Unable to find faculty profile for "+name+" at url "+url
+		else:
+			facultyUrl = links.eq(target).attr('href')
+			if facultyUrl is not None:
+				self.facultyMiner(facultyUrl)
+			else:
+				print "Faculty profile url is empty for "+name+" at url "+url
+
 	'''
 	Constructor that takes a parameter of the academics page url and extracts the url of each department and passes it to the departementMiner
 	'''
@@ -18,6 +53,8 @@ class Miner:
 
 		# find all the department details
 		links = d("#departmentDetails")
+
+		debug = 0
 
 		# loop through the links and find the divs in each
 		for i in range(0, links.length):
@@ -31,8 +68,15 @@ class Miner:
 				current = pq(divs.eq(j))
 				href = current('a').attr("href")
 
+				# check if we found a url
 				if href is not None:
-					print href
+					debug = debug + 1
+					self.departementMiner(href)
+
+					if debug == 1:
+						break
+				else:
+					print "Department url not found for element at"+current
 
 
 # start the miner
